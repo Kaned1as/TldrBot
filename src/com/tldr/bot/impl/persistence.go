74aa2ccb728ed11bf613839edd890e8cafb2631a
Oleg Chernovskiy <adonai@xaker.ru>
2016-11-27 20:08:16 +0300

    Fixes to regexes, started to implement persistence

---
+++ src/com/tldr/bot/impl/persistence.go
@@ -0,0 +1,93 @@
+package impl
+
+import (
+    "gopkg.in/gorp.v2"
+    _ "github.com/mattn/go-sqlite3"
+    "time"
+    "database/sql"
+    "log"
+    "os"
+)
+
+type Score struct {
+    Id uint64                           `db:"id, primarykey, autoincrement"`
+    // ID from Telegram
+    PersonId int64
+    // hit grade (4 minimum)
+    Grade uint8
+    // time when score was recorded
+    Time time.Time
+}
+
+type PersistenceLayer struct {
+    database *gorp.DbMap
+}
+
+func newDb(path string) *PersistenceLayer {
+    pl := PersistenceLayer{}
+    pl.database = initDb(path)
+    return &pl
+}
+
+func (pl *PersistenceLayer) Close() {
+    pl.database.Db.Close()
+}
+
+func (pl *PersistenceLayer) SaveScore(score *Score) {
+    pl.database.Insert(score)
+}
+
+func (pl *PersistenceLayer) GetScores(personId int64) (total int64, highest *Score, latest *Score) {
+    var maxTime time.Time
+
+    total, _ = pl.database.SelectInt("select sum(Grade) from scores where PersonId = ?", personId)
+    max, _ := pl.database.SelectInt("select max(Grade) from scores where PersonId = ?", personId)
+    _, err := pl.database.Select(&maxTime, "select max(Time) from scores where PersonId = ?", personId)
+    checkErr(err, "Failed to select latest time of all scores")
+
+    var maxScores, latestScores []*Score
+    _, err = pl.database.Select(&maxScores, "select * from scores where Grade = ?", max)
+    checkErr(err, "Failed to select max scores")
+    _, err = pl.database.Select(&latestScores, "select * from scores where Time = ?", maxTime)
+    checkErr(err, "Failed to select latest scores")
+
+    if len(maxScores) > 0 {
+        highest = maxScores[0]
+    }
+
+    if len(latestScores) > 0 {
+        latest = latestScores[0]
+    }
+
+    return
+}
+
+func initDb(path string) *gorp.DbMap {
+    // connect to db using standard Go database/sql API
+    // use whatever database/sql driver you wish
+    db, err := sql.Open("sqlite3", path)
+    checkErr(err, "sql.Open failed")
+
+    // construct a gorp DbMap
+    dbmap := &gorp.DbMap{Db: db, Dialect: gorp.SqliteDialect{}}
+    dbmap.TraceOn("[ORM]", log.New(os.Stdout, "TraceSQL:", log.Lmicroseconds))
+
+    // add a table, setting the table name to 'posts' and
+    // specifying that the Id property is an auto incrementing PK
+    scoreT := dbmap.AddTableWithName(Score{}, "scores")
+    scoreT.AddIndex("PersonIdx", "Btree", []string{"PersonId"})
+    scoreT.AddIndex("TimeIdx", "Btree", []string{"Time"})
+
+    // create the table. in a production system you'd generally
+    // use a migration tool, or create the tables via scripts
+    err = dbmap.CreateTablesIfNotExists()
+    checkErr(err, "Create tables failed")
+
+    return dbmap
+}
+
+func checkErr(err error, msg string) {
+    if err != nil {
+        log.Fatalln(msg, err)
+    }
+}
--- src/com/tldr/bot/impl/poller.go
+++ src/com/tldr/bot/impl/poller.go
@@ -13,52 +13,56 @@ import (
     "unicode/utf8"
     "io"
     "os"
+    "strings"
 )
 
 var /* const */ LEET_REGEX = []*regexp.Regexp {
-    regexp.MustCompile("0000(0*)"),
-    regexp.MustCompile("0123(4?5?6?7?8?9?)"),
-    regexp.MustCompile("1111(1*)"),
-    regexp.MustCompile("1234(5?6?7?8?9?)"),
-    regexp.MustCompile("1337"),
-    regexp.MustCompile("2222(2*)"),
-    regexp.MustCompile("2345(6?7?8?9?)"),
+    regexp.MustCompile("0000(0|00)?"), // more is pointless, there is no 00 day or month or year
+    regexp.MustCompile("0123(4|45)?"), // 6789 are pointless, we can match only HHmmss
+    regexp.MustCompile("1111(1|11|1111|111111)?"), // so max 1111111111 matches MMddHHmmss
+    regexp.MustCompile("1234(5|56)?"), // 789 are pointless, see #2
+    regexp.MustCompile("1337"), // best regexp, truly
+    regexp.MustCompile("2222(2|22|2222)?"), // so max 22222222 matches ddHHmmss
+    regexp.MustCompile("(01)?2345"), // first day of month - 012345 matches ddHHmm
 }
 
 // Poller for bot updates
 // repeatedly retrieves updates from the telegram server and updates its read position
 // Note: runs in its own thread
 type Poller struct {
-    client http.Client
-    Token string
-    lastId int64
+    client http.Client   // http client to operate on
+    Token string         // bot token
+    lastId int64         // last Telegram update ID we know
+    db *PersistenceLayer // database
+    scoresToday []Score
+    today time.Time
 }
 
 // starts poller
 func (poll *Poller) Start(waiter *sync.WaitGroup) {
+    // init
     poll.client = http.Client{Timeout: time.Second * 5}
     ticker := time.NewTicker(POLLING_INTERVAL)
+    poll.db = newDb("data/scores.db")
+    poll.today = time.Unix(0, 0)
+
     go poll.doWork(ticker, waiter)
 }
 
-func (this *Poller) doWork(tick *time.Ticker, waiter *sync.WaitGroup) {
+func (poll *Poller) doWork(ticker *time.Ticker, waiter *sync.WaitGroup) {
+    defer poll.db.Close()
     defer waiter.Done()
 
-    for time := range tick.C {
-        // create request
-        request := api.GetUpdatesRequest{Offset: this.lastId + 1}
-        body, _ := json.Marshal(request)
-        req, createErr := http.NewRequest("POST", API_ENDPOINT + this.Token + GET_UPDATES_PATH, bytes.NewBuffer(body))
-        if createErr != nil {
-            log.Fatal("Error creating http request, shutting down ..." + createErr.Error())
+    for tick := range ticker.C {
+        if tick.Day() > poll.today.Day() {
+            poll.scoresToday = []Score{} // empty the list for today's scores
         }
 
-        // call to server
-        req.Header.Set("Content-Type", "application/json")
-        resp, postErr := this.client.Do(req)
-        if postErr != nil {
-            fmt.Println("Something is wrong with Telegram servers ..." + postErr.Error())
-            continue
+        // create request
+        request := api.GetUpdatesRequest{Offset: poll.lastId + 1}
+        resp := poll.sendObject(request, API_ENDPOINT + poll.Token + GET_UPDATES_PATH)
+        if resp == nil {
+            continue // no response, should be logged
         }
 
         // parse response
@@ -71,9 +75,9 @@ func (this *Poller) doWork(tick *time.Ticker, waiter *sync.WaitGroup) {
         }
 
         for _, upd := range updates.Result {
-            this.handleUpdate(upd)
+            poll.handleUpdate(upd)
         }
-        fmt.Printf("time: %v, updates: %#v\n", time, updates);
+        fmt.Printf("time: %v, updates: %#v\n", tick, updates);
     }
 }
 
@@ -104,28 +108,61 @@ func (poll *Poller) handleUpdate(update api.Update) {
 }
 
 func (poll *Poller) handleL33t(msg *api.Message, scored string, regex *regexp.Regexp) {
-    timeStr := time.Now().Format("20060201150405")
+    currTime := time.Now()
+
+    hmTimeStr := currTime.Format("1504")
+    if !strings.HasPrefix(regex.String(), hmTimeStr) {
+        return // score must match HHmm
+    }
+                          //    yyyyMMddHHmmss
+    timeStr := currTime.Format("20060102150405")
     match := regex.FindStringSubmatch(timeStr)
     if match == nil {
         return // not enough to be l33t!
     }
+
+    // make sure person hasn't scored this one yet...
+    for _, score := range poll.scoresToday {
+        if score.PersonId == msg.From.Id && currTime.Sub(score.Time).Minutes() < 1 {
+            return  // diff between scores is less than minute? You dirty jackass!
+        }
+    }
+
+    // someone scored, save it and report
     scoredSize := utf8.RuneCountInString(scored) // e.g. we sent 111111 - wanted to score 11 seconds too, it'll be 6
     matchedSize := utf8.RuneCountInString(match[0]) // if time is 11:11:12 we'll get only 5
-    totalMatched := min(scoredSize, matchedSize)
+    totalMatched := uint8(min(scoredSize, matchedSize))
+
+    score := Score{PersonId: msg.From.Id, Grade: totalMatched, Time: currTime}
+    poll.scoresToday = append(poll.scoresToday, score) // save it to today's scores
+    poll.db.SaveScore(&score) // save it to DB
 
     disablePreview := new(bool); *disablePreview = true
     report := fmt.Sprintf("%s is a l33t now, scored %d points, current time is %s",
         msg.From.First_name,
         totalMatched,
-        time.Now().String())
+        currTime.String())
+
     request := api.SendMessage{
         Text: report,
         ChatId: msg.Chat.Id,
         ParseMode: "Markdown",
         ReplyToMessageId: &msg.Message_id,
         DisableWebPagePreview: disablePreview}
+
+    resp := poll.sendObject(request, API_ENDPOINT + poll.Token + SEND_MESSAGE_PATH)
+    if resp == nil {
+        return // no response, should be logged
+    }
+
+    defer resp.Body.Close()
+    fmt.Println(resp.Status)
+    io.Copy(os.Stdout, resp.Body)
+}
+
+func (poll *Poller) sendObject(request interface{}, url string) (*http.Response) {
     body, _ := json.Marshal(request)
-    req, createErr := http.NewRequest("POST", API_ENDPOINT + poll.Token + SEND_MESSAGE_PATH, bytes.NewBuffer(body))
+    req, createErr := http.NewRequest("POST", url, bytes.NewBuffer(body))
     if createErr != nil {
         log.Fatal("Error creating http request, shutting down ..." + createErr.Error())
     }
@@ -135,10 +172,8 @@ func (poll *Poller) handleL33t(msg *api.Message, scored string, regex *regexp.Re
     resp, postErr := poll.client.Do(req)
     if postErr != nil {
         fmt.Println("Something is wrong with Telegram servers ..." + postErr.Error())
-        return
+        return nil
     }
 
-    defer resp.Body.Close()
-    fmt.Println(resp.Status)
-    io.Copy(os.Stdout, resp.Body)
+    return resp
 }
